d458ddc98c4573963e8b1b65302913acf1c20061
anderson <anderson.tom.leite@gmail.com>
2022-08-03 15:23:29 -0300

    adicionando saldo

--- Prolog/cliente.pl
+++ Prolog/cliente.pl
@@ -2,13 +2,15 @@ setup_bd :-
 	consult('./data/bd_clientes.pl').
 
 arquivo_vazio :-
-	\+(predicate_property(cliente(_,_,_,_), dynamic)).
+	\+(predicate_property(cliente(_,_,_,_,_), dynamic)).
 
 adicionaCliente :-
 	setup_bd,
 	tell('./data/bd_clientes.pl'), nl,
-	listing(cliente/4),
+	listing(cliente/5),
 	told.
+
+
 
 cadastraCliente :-
 	setup_bd,
@@ -20,15 +22,17 @@ cadastraCliente :-
 	read_line_to_string(user_input, Senha),
 	nl, writeln("Insira seu telefone: "),
 	read_line_to_string(user_input, Telefone),
+	nl, writeln("Insira saldo inicial: "),
+	read_line_to_string(user_input, Saldo),
 	nl,
-	(get_emails_clientes(Emails), member(Cpf, Emails) -> nl, writeln("Cpf já cadastrado."), nl;
-	assertz(cliente(Nome, Cpf, Senha, Telefone)),
+	(get_cpf_clientes(Cpfs), member(Cpf, Cpfs) -> nl, writeln("Cpf já cadastrado."), nl;
+	assertz(cliente(Nome, Cpf, Senha, Telefone, Saldo)),
 	adicionaCliente,
 	writeln("Cliente cadastrado com sucesso!"),nl),
 	fimMetodo.
 
-get_emails_clientes(Emails) :- 
-	findall(Cpf, cliente(_,Cpf,_,_), Emails).
+get_cpf_clientes(Cpfs) :- 
+	findall(Cpf, cliente(_,Cpf,_,_,_), Cpfs).
 
 loginCliente(Cpf) :-
 	nl,
@@ -36,27 +40,28 @@ loginCliente(Cpf) :-
 	read_line_to_string(user_input, Cpf),
 	writeln("Insira sua senha: "),
 	read_line_to_string(user_input, Senha),
-	(cliente(_, Cpf, Senha, _) -> nl, writeln("Login realizado com sucesso!"), nl;
+	(cliente(_, Cpf, Senha, _,_) -> nl, writeln("Login realizado com sucesso!"), nl;
 	writeln("Senha incorreta."), nl, false).
 
 login_cliente(Cpf) :-
 	setup_bd,
 	arquivo_vazio -> writeln("Cliente não cadastrado."), nl, false;
-	(cliente(_, _, _, _) -> loginCliente(Cpf);
+	(cliente(_, _, _, _, _) -> loginCliente(Cpf);
 	writeln("Cliente não cadastrado."), nl, false),
 	fimMetodo.
 
 consultaConta(Cpf) :-
 	setup_bd_cliente,
-	bagof(Nome, cliente(Nome, Cpf, _, _), ClienteName),
-	bagof(Cpf, cliente(_, Cpf, _, _), ClienteCpf),
-	bagof(Telefone, cliente(_, Cpf, _, Telefone), ClienteTelefone),
-	writeln("Dados da sua conta: "),
+	bagof(Nome, cliente(Nome, Cpf, _, _, _), ClienteName),
+	bagof(Cpf, cliente(_, Cpf, _, _, _), ClienteCpf),
+	bagof(Telefone, cliente(_, Cpf, _, Telefone, _), ClienteTelefone),
+	bagof(Saldo, cliente(_, Cpf, _, _, Saldo), ClienteSaldo),
+	writeln("Dados da conta: "),
 	exibeNomeCliente(ClienteName),
 	exibeCpfCliente(ClienteCpf),
 	exibeTelefoneCliente(ClienteTelefone),
-	told, nl,
-	fimMetodo.
+	exibeSaldoCliente(ClienteSaldo),
+	told, nl, printLine.
 
 
 fimMetodo:-
